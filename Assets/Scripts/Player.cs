9c6f5ebce38d33c97ce78fdd47c699c97590c4f2
caneslotocato <caneslotocato1@gmail.com>
2016-06-01 22:29:57 +0200

    ri-commit del player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-    
-	public GameObject soundObj;
-	public GameObject wavePrefab;
-	public float speed;
-	public float coolDown;
+public class Player : MonoBehaviour
+{
+
+    public GameObject soundObj;
+    public GameObject wavePrefab;
+    public float speed;
+    public float coolDown;
     public float waveVelocity;
     public float waveDuration;
     public Transform camera;
     public Transform cameraPos;
     public Transform lookAt;
-	public Transform spawnPointWave;
+    public Transform spawnPointWave;
     public float maxVertical;
     [HideInInspector]
     public float xl;
@@ -26,27 +27,29 @@ public class Player : MonoBehaviour {
     private Rigidbody rb;
     private float startTimeShoot;
     private RaycastHit hit;
-	private LineRenderer myAimPreview;
+    private LineRenderer myAimPreview;
     private bool joyPad;
-   
+
 
     void Awake()
     {
         //lookAt.position = transform.position;
         rb = GetComponent<Rigidbody>();
         Physics.queriesHitTriggers = false;
-		myAimPreview = GetComponent<LineRenderer> ();
+        myAimPreview = GetComponent<LineRenderer>();
     }
 
-	// Use this for initialization
-	void Start () {
-		myAimPreview.SetPosition (0, spawnPointWave.position);
-		myAimPreview.SetPosition (1, spawnPointWave.forward * waveVelocity * waveDuration);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       // cameraPos.position = camera.position;
+    // Use this for initialization
+    void Start()
+    {
+        myAimPreview.SetPosition(0, spawnPointWave.position);
+        myAimPreview.SetPosition(1, spawnPointWave.forward * waveVelocity * waveDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // cameraPos.position = camera.position;
         xl = Input.GetAxis("RightAnalX");
         yl = Input.GetAxis("RightAnalY");
         x = Input.GetAxis("LeftAnalX");
@@ -93,29 +96,29 @@ public class Player : MonoBehaviour {
                 MakeSound();
             }
 
-          
+
 
             //Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1 << LayerMask.NameToLayer("Floor"));
             //Vector3 mouse2World = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Vector3.Distance(Camera.main.transform.position, hit.transform.position), Input.mousePosition.z));
             //Debug.DrawLine(transform.position, hit.point, Color.red, 1);
 
             //transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
-            
+
         }
         myAimPreview.SetPosition(0, spawnPointWave.position);
         myAimPreview.SetPosition(1, spawnPointWave.position + spawnPointWave.forward * waveVelocity * waveDuration);
-        
+
     }
 
     void Move(Vector3 direction)
     {
-        rb.MovePosition(transform.position + direction*speed);
+        rb.MovePosition(transform.position + direction * speed);
     }
 
     void ShootSoundWave()
     {
         GameObject wave = Instantiate(wavePrefab, spawnPointWave.position, spawnPointWave.rotation * wavePrefab.transform.rotation) as GameObject;
-        wave.SendMessage("Shoot", new float[]{ waveVelocity, waveDuration });
+        wave.SendMessage("Shoot", new float[] { waveVelocity, waveDuration });
     }
 
     void MakeSound()
@@ -126,7 +129,7 @@ public class Player : MonoBehaviour {
 
     void MoveJoyPad()
     {
-       
+
         if (x > 0.1)
         {
             Vector3 s = new Vector3(transform.position.x + (x * (speed + 0.3f)), transform.position.y, transform.position.z - (y * (speed + 0.3f)));
@@ -151,30 +154,30 @@ public class Player : MonoBehaviour {
     }
 
 
-   /* void RotateJoyPad()
-    {
-      
-        if (xl > 0.1)
-        {
-            lookAt.position = new Vector3(transform.position.x + yl, transform.position.y, transform.position.z - yl);
-            transform.LookAt(lookAt);
-        }
-        if (xl < -0.1)
-        {
-            lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
-            transform.LookAt(lookAt);
-        }
-        if (yl > 0.1)
-        {
-            lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
-            transform.LookAt(lookAt);
-        }
-        if (yl < -0.1)
-        {
-            lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
-            transform.LookAt(lookAt);
-        }
-    }*/
+    /* void RotateJoyPad()
+     {
+
+         if (xl > 0.1)
+         {
+             lookAt.position = new Vector3(transform.position.x + yl, transform.position.y, transform.position.z - yl);
+             transform.LookAt(lookAt);
+         }
+         if (xl < -0.1)
+         {
+             lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
+             transform.LookAt(lookAt);
+         }
+         if (yl > 0.1)
+         {
+             lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
+             transform.LookAt(lookAt);
+         }
+         if (yl < -0.1)
+         {
+             lookAt.position = new Vector3(transform.position.x + xl, transform.position.y, transform.position.z - yl);
+             transform.LookAt(lookAt);
+         }
+     }*/
 
     void CameraRotate()
     {
@@ -182,7 +185,7 @@ public class Player : MonoBehaviour {
         /*Vector3 pos = new Vector3(cameraPos.position.x - transform.position.x, lookAt.position.y, cameraPos.position.z - transform.position.z);
         
         lookAt.position = new Vector3(-pos.x, pos.y, -pos.z);*/
-        
+
         lookAt.position = new Vector3(cameraPos.position.x, Mathf.Clamp(cameraPos.position.y, (-maxVertical / 10), maxVertical), cameraPos.position.z);
         transform.LookAt(lookAt);
     }
