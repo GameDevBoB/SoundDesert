352384983dbe046ac3c3e34d11e4db0d40735471
asddas <asdasdasd>
2016-06-01 17:53:41 +0200

    Sliding Doors and buttons

---
+++ Assets/Scripts/ButtonObj.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonObj : MonoBehaviour
+{
+
+    public GameObject door;
+   
+
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            door.SendMessage("Open");
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            door.SendMessage("Close");
+        }
+    }
+
+}
