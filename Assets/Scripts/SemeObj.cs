8a110d99f29878a65a702b0c2993746e4b2dc6f8
Dariodefra <dariodefra@gmail.com>
2016-07-06 18:02:34 +0200

    Seme

---
+++ Assets/Scripts/SemeObj.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SemeObj : MonoBehaviour
+{
+    private Animator myAnim;
+
+    private int requiredButtonNumber;
+    private int pressedButtonsNumber;
+
+    void Awake()
+    {
+        myAnim = GetComponent<Animator>();
+        requiredButtonNumber = 0;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        pressedButtonsNumber = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Open()
+    {
+        pressedButtonsNumber++;
+        if ((pressedButtonsNumber == requiredButtonNumber))
+        {
+            myAnim.SetTrigger("Bloom");
+        }
+    }
+
+    public void Close()
+    {
+        pressedButtonsNumber--;
+    }
+
+    public void AddRequiredButton()
+    {
+        requiredButtonNumber++;
+        //Debug.Log(requiredButtonNumber);
+    }
+}
